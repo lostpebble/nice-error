f132d096cfec299e6485fffd30b94308e28898f6
Paul <lostpebble@protonmail.com>
2026-04-08 02:36:33 +0200

    feat: adding build and demo

---
+++ packages/core/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./NiceError/NiceError";
+export * from "./NiceError/NiceError.types";
+export * from "./NiceErrorDefined/defineNiceError";
+export * from "./NiceErrorDefined/NiceErrorDefined";
+export * from "./utils/castNiceError";
+export * from "./utils/isNiceErrorObject";
+export * from "./utils/isRegularErrorObject";
